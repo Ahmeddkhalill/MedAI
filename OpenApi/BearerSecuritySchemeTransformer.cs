ac4dffef1de2ba58a56ae13d8a919fe6585aa7fd
Ahmed Khalil <136190754+Ahmeddkhalill@users.noreply.github.com>
2026-01-20 15:24:40 +0200

    Update Project to Dotnet 10

--- OpenApi/BearerSecuritySchemeTransformer.cs
+++ OpenApi/BearerSecuritySchemeTransformer.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.OpenApi;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 namespace MedAI.OpenApi;
 
@@ -12,7 +12,7 @@ public sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvide
         var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
         if (authenticationSchemes.Any(authScheme => authScheme.Name == JwtBearerDefaults.AuthenticationScheme))
         {
-            var requirements = new Dictionary<string, OpenApiSecurityScheme>
+            var requirements = new Dictionary<string, IOpenApiSecurityScheme>
             {
                 ["Bearer"] = new OpenApiSecurityScheme
                 {
@@ -27,11 +27,12 @@ public sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvide
             document.Components.SecuritySchemes = requirements;
 
             // Apply it as a requirement for all operations
-            foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations))
+            foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations!))
             {
+                operation.Value.Security ??= [];
                 operation.Value.Security.Add(new OpenApiSecurityRequirement
                 {
-                    [new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } }] = Array.Empty<string>()
+                    [new OpenApiSecuritySchemeReference("Bearer",document)] = []
                 });
             }
         }
